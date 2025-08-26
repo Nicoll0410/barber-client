4fb16dfd78170c10ea6a00193dbd3b7cbd6e8edc
Nicoll Giraldo <nicollgiraldo0410@gmail.com>
2025-08-26 08:50:20 -0500

    dependencias 1.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,19 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
-const webpack = require('webpack');
 
 module.exports = async function(env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
 
-  // Alias
-  config.resolve.alias['react-native$'] = 'react-native-web';
-
-  // Polyfills Node
+  // Polyfills para Node.js core modules
   config.resolve.fallback = {
+    ...config.resolve.fallback,
     crypto: require.resolve('crypto-browserify'),
+    vm: require.resolve('vm-browserify'),
     stream: require.resolve('stream-browserify'),
     assert: require.resolve('assert'),
-    http: require.resolve('stream-http'),
-    https: require.resolve('https-browserify'),
-    os: require.resolve('os-browserify/browser'),
-    url: require.resolve('url'),
     buffer: require.resolve('buffer'),
-    vm: false
+    process: require.resolve('process/browser'),
+    util: require.resolve('util')
   };
-
-  // Plugins
-  config.plugins.push(
-    new webpack.ProvidePlugin({
-      Buffer: ['buffer', 'Buffer'],
-      process: 'process/browser'
-    })
-  );
 
   return config;
 };
